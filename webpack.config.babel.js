4d983575465e74999045ec795e36ccd08bc5fad4
Thadeu Luz <thadeuluz@gmail.com>
2017-03-17 13:56:15 -0300

    Use babel with webpack

---
+++ webpack.config.babel.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config = {
+  entry: './src/index.jsx',
+  resolve: { extensions: ['.js', '.jsx'] },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [{ loader: 'babel-loader', query: { compact: false } }],
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
+};
+
+export default config;
